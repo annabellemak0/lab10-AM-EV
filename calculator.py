331a1bc1b0b7b8949aa63478318b82097565c731
annabellemak0 <annabelle.s.mak@gmail.com>
2025-04-09 13:15:09 -0400

    modidied calculator

--- calculator.py
+++ calculator.py
@@ -8,6 +8,15 @@ One function per operation, in order.
 
 import math
 
+def square_root(a):
+    if a <0:
+        raise ValueError
+    return math.sqrt(a)
+
+def hypotenuse(a, b):
+    return math.hypot(a, b)
+
+
 def add(a, b): 
     c= a + b
     return c
